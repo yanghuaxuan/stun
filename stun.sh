951f3ce61b2a3467901afa7fd1cfbe54e817b521
yanghuaxuan <hyang@hyang.xyz>
2023-09-07 18:53:58 -0700

    parameterize

--- stun.sh
+++ stun.sh
@@ -3,6 +3,9 @@
 NS="split"
 BR="br0"
 IF="eno1"
+BR_IP="10.1.1.1" BR_MASK="24"
+VETH_TO="veth0" VETH_FROM="veth1"
+VETH_IP="10.1.1.2" VETH_MASK="24"
 
 create() {
   # Create namespace
@@ -12,35 +15,32 @@ create() {
   ip netns exec "$NS" ip link set lo up
 
   # Create veth pairs (veth0, veth1)
-  ip link add veth0 type veth peer name veth1
+  ip link add "$VETH_TO" type veth peer name "$VETH_FROM"
 
-  ip link set veth0 up
+  ip link set "$VETH_TO" up
 
-  # Moving veth1 to namespace
-  ip link set veth1 netns "$NS"
-
-  # Set veth1 as default route inside namespace
-  # ip netns exec "$NS" ip route add default dev veth1 via 10.1.1.1
+  # Moving $VETH_FROM to namespace
+  ip link set "$VETH_FROM" netns "$NS"
 
   # Create bridge
   ip link add "$BR" type bridge
 
-  # Add veth0 to bridge
-  ip link set veth0 master "$BR"
+  # Add $VETH_TO to bridge
+  ip link set "$VETH_TO" master "$BR"
 
   # Set IPs
-  ip addr add 10.1.1.1/24 dev "$BR"
-  ip netns exec "$NS" ip addr add 10.1.1.2/24 dev veth1
+  ip addr add "$BR_IP/$BR_MASK" dev "$BR"
+  ip netns exec "$NS" ip addr add "$VETH_IP/$VETH_MASK" dev "$VETH_FROM"
 
   ip link set "$BR" up
-  ip netns exec "$NS" ip link set veth1 up
-  ip -n "$NS" route add default dev veth1 via 10.1.1.1
+  ip netns exec "$NS" ip link set "$VETH_FROM" up
+  ip -n "$NS" route add default dev veth1 via "$BR_IP"
 
   # Don't route to wg
-  ip rule add from 10.1.1.2 table main priority 99
+  ip rule add from "$VETH_IP" table main priority 99
 
   # IPTables NAT
-  iptables -t nat -A POSTROUTING -s 10.1.1.1/24 -o "$IF" -j MASQUERADE
+  iptables -t nat -A POSTROUTING -s "$BR_IP/$BR_MASK" -o "$IF" -j MASQUERADE
   iptables -A FORWARD -i "$IF" -o "$BR" -j ACCEPT
   iptables -A FORWARD -o "$IF" -i "$BR" -j ACCEPT
 
@@ -52,13 +52,13 @@ create() {
 destroy() {
   rm -Rf /etc/netns/"$NS"
 
-  iptables -t nat -D POSTROUTING -s 10.1.1.1/24 -o "$IF" -j MASQUERADE
+  iptables -t nat -D POSTROUTING -s "$BR_IP/$BR_MASK" -o "$IF" -j MASQUERADE
   iptables -D FORWARD -i "$IF" -o "$BR" -j ACCEPT
   iptables -D FORWARD -o "$IF" -i "$BR" -j ACCEPT
 
-  ip rule delete from 10.1.1.2 table main priority 99
+  ip rule delete from "$VETH_IP" table main priority 99
 
-  ip link delete veth0
+  ip link delete "$VETH_TO"
   ip link delete "$BR"
 
   ip netns delete "$NS"
